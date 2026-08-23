f4e4cefe338426a573aa4dd96900ca460b559583
dceoy <1938249+dceoy@users.noreply.github.com>
2026-08-23 23:08:49 +0900

    Derive plugin pins from active g:plugs, use tagger dates

--- install_latest_vim.sh
+++ install_latest_vim.sh
@@ -114,16 +114,24 @@ function github_commit_before {
 }
 
 function github_tag_date {
-  local repository="${1}" tag="${2}" sha="${3}" reference object_type object_sha
+  local repository="${1}" tag="${2}" sha="${3}" reference object_type object_sha tag_object
   reference="$(github_api "https://api.github.com/repos/${repository}/git/ref/tags/${tag}")"
   object_type="$(jq -er '.object.type' <<< "${reference}")"
+  object_sha="$(jq -er '.object.sha' <<< "${reference}")"
   if [[ "${object_type}" = 'tag' ]]; then
-    object_sha="$(jq -er '.object.sha' <<< "${reference}")"
-    github_api "https://api.github.com/repos/${repository}/git/tags/${object_sha}" \
-      | jq -er '.tagger.date'
-  else
-    github_api "https://api.github.com/repos/${repository}/commits/${sha}" \
+    tag_object="$(github_api "https://api.github.com/repos/${repository}/git/tags/${object_sha}")"
+    [[ "$(jq -er '.object.type' <<< "${tag_object}")" = 'commit' ]] \
+      || abort "tag does not point to a commit: ${repository} ${tag}"
+    [[ "$(jq -er '.object.sha' <<< "${tag_object}")" = "${sha}" ]] \
+      || abort "tag target changed while resolving: ${repository} ${tag}"
+    jq -er '.tagger.date' <<< "${tag_object}"
+  elif [[ "${object_type}" = 'commit' ]]; then
+    [[ "${object_sha}" = "${sha}" ]] \
+      || abort "tag target changed while resolving: ${repository} ${tag}"
+    github_api "https://api.github.com/repos/${repository}/commits/${object_sha}" \
       | jq -er '.commit.committer.date'
+  else
+    abort "unsupported tag object type: ${repository} ${tag}"
   fi
 }
 
@@ -163,7 +171,7 @@ function resolve_vim_version {
 function update_vim_plugins {
   local vim_autoload_dir="${VIM_DIR}/autoload"
   local vim_plug_vim="${vim_autoload_dir}/plug.vim"
-  local vim_plug_tmp repository sha name
+  local vim_plug_tmp discovery discovery_vim repository sha name
 
   [[ -f "${VIMRC}" ]] || abort "vimrc not found: ${VIMRC}"
   [[ -x "${VIM_BIN_DIR}/vim" ]] || abort "vim not found or not executable: ${VIM_BIN_DIR}/vim"
@@ -182,15 +190,75 @@ function update_vim_plugins {
   mv -f "${vim_plug_tmp}" "${vim_plug_vim}"
 
   PINS=$(mktemp "${TMPDIR:-/tmp}/vim-plug-pins.XXXXXX")
-  trap 'rm -f "${PINS}"' EXIT
-  sed -nE "s/^[[:space:]]*Plug[[:space:]]+['\"]([[:alnum:]_.-]+\/[[:alnum:]_.-]+)['\"][[:space:]]*(\".*)?$/\1/p" "${VIMRC}" \
-    | while IFS= read -r repository; do
-      repository="${repository%.git}"
-      sha="$(github_commit_before "${repository}")"
-      name="${repository##*/}"
-      printf "if has_key(g:plugs, '%s')\n  let g:plugs['%s'].commit = '%s'\nendif\n" \
-        "${name}" "${name}" "${sha}"
-    done > "${PINS}"
+  discovery=$(mktemp "${TMPDIR:-/tmp}/vim-plug-discovery.XXXXXX")
+  discovery_vim=$(mktemp "${TMPDIR:-/tmp}/vim-plug-discovery.XXXXXX.vim")
+  trap 'rm -f "${PINS}" "${discovery}" "${discovery_vim}"' EXIT
+  cat > "${discovery_vim}" << 'VIM'
+if !exists('g:plugs')
+  call writefile([], $VIM_PLUG_DISCOVERY)
+  qa!
+endif
+
+let s:baseline_name = '__codex_default_baseline__'
+while has_key(g:plugs, s:baseline_name)
+  let s:baseline_name .= '_'
+endwhile
+call plug#('vim-plug/' . s:baseline_name)
+let s:baseline = copy(g:plugs[s:baseline_name])
+let s:lines = []
+
+for [s:name, s:spec] in items(g:plugs)
+  if s:name ==# s:baseline_name
+    continue
+  endif
+  let s:match = matchlist(get(s:spec, 'uri', ''), '^https://github\.com/\([[:alnum:]_.-]\+\)/\([[:alnum:]_.-]\+\)\%(\.git\)\?$')
+  if empty(s:match) || s:name !=# s:match[2]
+    continue
+  endif
+
+  let s:default_dir = fnamemodify(get(s:baseline, 'dir', ''), ':h') . '/' . s:name
+  if get(s:spec, 'dir', '') !=# s:default_dir
+    continue
+  endif
+
+  let s:actual = copy(s:spec)
+  let s:expected = copy(s:baseline)
+  call remove(s:actual, 'dir')
+  call remove(s:actual, 'uri')
+  call remove(s:expected, 'dir')
+  call remove(s:expected, 'uri')
+  if len(keys(s:actual)) != len(keys(s:expected))
+    continue
+  endif
+  let s:is_default = 1
+  for s:key in keys(s:expected)
+    if !has_key(s:actual, s:key) || string(s:actual[s:key]) !=# string(s:expected[s:key])
+      let s:is_default = 0
+      break
+    endif
+  endfor
+  if s:is_default
+    call add(s:lines, s:name . "\t" . s:match[1] . '/' . s:match[2])
+  endif
+endfor
+
+call remove(g:plugs, s:baseline_name)
+call writefile(s:lines, $VIM_PLUG_DISCOVERY)
+qa!
+VIM
+  if ! VIM_PLUG_DISCOVERY="${discovery}" "${VIM_BIN_DIR}/vim" -N -u "${VIMRC}" -U NONE -i NONE -e -s \
+    -S "${discovery_vim}" -c 'qa!'; then
+    return 1
+  fi
+  LC_ALL=C sort -u "${discovery}" -o "${discovery}"
+  while IFS=$'\t' read -r name repository; do
+    [[ -n "${name}" && -n "${repository}" ]] || continue
+    [[ "${name}" =~ ^[[:alnum:]_.-]+$ ]] || continue
+    [[ "${repository}" =~ ^[[:alnum:]_.-]+/[[:alnum:]_.-]+$ ]] || continue
+    sha="$(github_commit_before "${repository}")"
+    printf "if has_key(g:plugs, '%s')\n  let g:plugs['%s'].commit = '%s'\nendif\n" \
+      "${name}" "${name}" "${sha}"
+  done < "${discovery}" > "${PINS}"
 
   "${VIM_BIN_DIR}/vim" -N -u "${VIMRC}" -U NONE -i NONE -e -s \
     -S "${PINS}" -c 'PlugUpdate --sync | qa'
